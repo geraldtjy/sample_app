4f7515efc4b2058bb3459071146dd2599fb5be16
Gerald <geraldtjy@gmail.com>
2010-09-09 20:27:33 +0800

    Update to ctonrollers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  # Function designed for use in views.
+  # Return a title on a per-page basis.
+  # This is a helper method (no '@' sign).
+  def title
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+
 end
